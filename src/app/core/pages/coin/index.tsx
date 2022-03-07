7fdcfe05c1777521d3751f9d7b465ddb18ee7fc2
Felipe Martins <mfelipeluiz99@gmail.com>
2022-03-07 16:37:06 -0300

    impl: creating page coin index and crypto json data

---
+++ src/app/core/pages/coin/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, FormEvent } from 'react'
+
+import { Panel, Typography, TypographyVariant } from 'components/atoms'
+import { Button, Letter, Price, Quantity, Select } from 'components/molecules'
+
+import coins from '../../../../data/data.json'
+import styles from './styles.module.scss'
+
+interface ICoin {
+  label: string
+  value: {
+    id: number
+    name: string
+    description: string
+    price: number
+  }
+}
+
+const Coin = (): JSX.Element => {
+  const [coin, setCoin] = useState<string>()
+  const [price, setPrice] = useState<number>()
+  const [description, setDescription] = useState<string>()
+
+  useEffect(() => {
+    const foundCoin: ICoin | undefined = coins.find(curr => curr.label === coin)
+    if (foundCoin) {
+      setPrice(foundCoin.value.price)
+      setDescription(foundCoin.value.description)
+    }
+  }, [coin])
+
+  return (
+    <div className={styles.container}>
+      <Typography text="Add New Coin" variant={TypographyVariant.h3} />
+      <Panel className={styles.panel}>
+        <div className={styles.containerAll}>
+          <div className={styles.containerInputs}>
+            <div className={styles.containerCoins}>
+              <Select
+                onChange={(e: FormEvent<HTMLSelectElement>): void =>
+                  setCoin(e.currentTarget.value)
+                }
+                filled={coin}
+                coins={coins}
+              />
+
+              <Price price={price} />
+
+              <Quantity />
+            </div>
+            <Letter coin={coin} description={description} />
+          </div>
+          <Button>ADD NEW COIN</Button>
+        </div>
+      </Panel>
+    </div>
+  )
+}
+
+export default Coin

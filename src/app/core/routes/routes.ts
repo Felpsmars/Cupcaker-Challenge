c77e0d3e442d4e13a77c66499969886c1994d5a1
Felipe Martins <mfelipeluiz99@gmail.com>
2022-03-07 16:38:39 -0300

    impl: adding routes for coin page

--- src/app/core/routes/routes.ts
+++ src/app/core/routes/routes.ts
@@ -1,7 +1,10 @@
-import { AppRoute } from './types'
+import Coin from 'app/core/pages/coin'
 import Home from 'app/core/pages/home'
+
+import { AppRoute } from './types'
 
 export const coreRoutes: AppRoute[] = [
   { path: '/', exact: true, component: Home },
+  { path: '/coin', exact: true, component: Coin },
   { path: '/private', exact: true, component: Home, isPrivate: true },
 ]
--- src/app/core/routes/types/index.ts
+++ src/app/core/routes/types/index.ts
@@ -6,13 +6,14 @@ import * as H from 'history'
 export type RouteProps = {
   location?: H.Location
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any> | React.FunctionComponent<any>
+  component:
+    | React.ComponentType<RouteComponentProps<any>>
+    | React.ComponentType<any>
+    | React.FunctionComponent<any>
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   render?: (props: RouteComponentProps<any>) => React.ReactNode
-  children?:
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    | ((props: RouteChildrenProps<any>) => React.ReactNode)
-    | React.ReactNode
+  children?: // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  ((props: RouteChildrenProps<any>) => React.ReactNode) | React.ReactNode
   path?: string
   exact?: boolean
   sensitive?: boolean
@@ -21,14 +22,17 @@ export type RouteProps = {
 
 export interface AppRoute {
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any> | React.FunctionComponent<any>,
-  path: string,
-  exact?: boolean,
-  isPrivate?: boolean,
+  component:
+    | React.ComponentType<RouteComponentProps<any>>
+    | React.ComponentType<any>
+    | React.FunctionComponent<any>
+  path: string
+  exact?: boolean
+  isPrivate?: boolean
 }
 
 export interface ModuleRouteProps {
-  routePrefix?: string,
-  routes: AppRoute[],
+  routePrefix?: string
+  routes: AppRoute[]
   isAuthenticated: () => boolean
 }

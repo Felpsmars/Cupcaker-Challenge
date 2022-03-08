8e8a6872645444d8bd48016d3f55b6ca3d2a20be
Felipe Martins <mfelipeluiz99@gmail.com>
2022-03-08 16:37:04 -0300

    refac: refactoring button component css

--- src/components/molecules/button/index.tsx
+++ src/components/molecules/button/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 
 import styles from './styles.module.scss'
 
 interface IProps {
+  className: string
   children: string
+  onClick: (e: FormEvent<HTMLButtonElement>) => void
 }
 
 const Button = (props: IProps): JSX.Element => {
   return (
-    <button className={styles.button} type="button">
+    <button
+      onClick={props.onClick}
+      id={styles.button}
+      className={props.className}
+      type="button"
+    >
       {props.children}
     </button>
   )

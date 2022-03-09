09ee75572a6f2f4e8952941eb1676c62ba1912ee
Felipe Martins <mfelipeluiz99@gmail.com>
2022-03-09 19:56:50 -0300

    task008: Rendering cards on home page to each object saved o coin page. Adding button to redirect to coin page

--- src/app/core/pages/home/index.tsx
+++ src/app/core/pages/home/index.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import { Panel, Typography, TypographyVariant } from 'components/atoms'
-import { EmptyState, EmptyStateType } from 'components/molecules'
+import { Button, EmptyState, EmptyStateType } from 'components/molecules'
+import { CryptoRows } from 'components/molecules/crypto-rows'
+
+import watchListContext from 'app/provider/watchListContext'
 
 import styles from './styles.module.scss'
 
 const Home = (): JSX.Element => {
+  const { data } = useContext(watchListContext)
+  const history = useHistory()
+
   return (
     <div className={styles.container}>
-      <Typography text="Watchlist" variant={TypographyVariant.h3} />
+      <div className={styles.overPanel}>
+        <Typography text="Watchlist" variant={TypographyVariant.h3} />
+        <Button
+          className={styles.button}
+          onClick={(): void => history.push('/coin')}
+        >
+          ADD NEW COIN
+        </Button>
+      </div>
       <Panel className={styles.panel}>
-        <EmptyState type={EmptyStateType.emptyWallet} />
+        {data.length < 1 ? (
+          <EmptyState type={EmptyStateType.emptyWallet} />
+        ) : (
+          <CryptoRows data={data} />
+        )}
       </Panel>
     </div>
   )
